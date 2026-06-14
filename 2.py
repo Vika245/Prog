72be70a2a73995d8a171e9c9389893bc1389e81b
Vika245 <V_ivanova_8700@mail.ru>
2026-06-15 01:45:17 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,5 @@
+prices = [1500, 500, 2000, 3500, 1000, 4500]
+print("Самый дорогой товар: ", max(prices))
+print("Самый дешевый товар: ", min(prices))
+print("Общая стоимость: ", sum(prices))
+print("Средняя цена товара: ", sum(prices) // len(prices))
